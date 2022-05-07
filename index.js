a3c832ac34ecefb04d1c4a65be18fbb9833414e7
benoah <noahbenaas@gmail.com>
2022-05-07 12:43:59 +0200

    update

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+let message = " "
+// 1. Declare a variable called message and assign its value to an empty string
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 2 
-let  secondCard = 11
-
-let sum = firstCard + secondCard
-console.log(sum);
-
-
-if (sum < 21) {
-    console.log("not far away ");
-}else if (sum === 21) {
-    console.log("winnner");
-}else if (sum > 21) {
-    console.log("loser  ");
-}
-
-
-// Check if the person is old enough to enter the nightclub (21)
-// Log a suitable message to the console in both cases
-
-let age = 22
-
-// if less than 21 -> "You can not enter the club!"
-// else            -> "Welcome!"
-
-
-if (age < 21) {
-    console.log("not welcomen");
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂" 
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-    console.log(" welcomen");
-    
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
 
-console.log(age);
+// 3. Log it out!
+console.log(message);
